5ddf1a0e47bef65a04ca371ba561b807256d914c
Leigh Halliday <leighhalliday@gmail.com>
2021-03-14 16:26:19 -0400

    Finished

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,66 @@ import {
   Tooltip,
   CartesianGrid,
 } from "recharts";
-import { format, parse, parseISO, subDays } from "date-fns";
+import { format, parseISO, subDays } from "date-fns";
 
 const data = [];
+for (let num = 30; num >= 0; num--) {
+  data.push({
+    date: subDays(new Date(), num).toISOString().substr(0, 10),
+    value: 1 + Math.random(),
+  });
+}
 
 export default function Home() {
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <AreaChart data={data}>
+        <defs>
+          <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+            <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+          </linearGradient>
+        </defs>
+
+        <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
+
+        <XAxis
+          dataKey="date"
+          axisLine={false}
+          tickLine={false}
+          tickFormatter={(str) => {
+            const date = parseISO(str);
+            if (date.getDate() % 7 === 0) {
+              return format(date, "MMM, d");
+            }
+            return "";
+          }}
+        />
+
+        <YAxis
+          datakey="value"
+          axisLine={false}
+          tickLine={false}
+          tickCount={8}
+          tickFormatter={(number) => `$${number.toFixed(2)}`}
+        />
+
+        <Tooltip content={<CustomTooltip />} />
+
+        <CartesianGrid opacity={0.1} vertical={false} />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
+
+function CustomTooltip({ active, payload, label }) {
+  if (active) {
+    return (
+      <div className="tooltip">
+        <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4>
+        <p>${payload[0].value.toFixed(2)} CAD</p>
+      </div>
+    );
+  }
+  return null;
 }
